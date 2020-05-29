b1089747e0071679a6fa97cb44e3fd149fb27c86
Tanay Raj <tanay.raj76@gmail.com>
2020-05-29 18:19:51 +0530

    initial

---
+++ public/register.js
@@ -0,0 +1,12 @@
+var modal = document.getElementById('id01');
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+var modal = document.getElementById('id01');
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
